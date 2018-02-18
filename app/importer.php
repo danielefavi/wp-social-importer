2bb5cf8e0d9d7d184871235b9c58ad7cab99770c
Daniele <dan.favi@gmail.com>
2018-02-18 17:41:59 +0100

    Renamed class Importer --> WpSocialImporter

--- app/importer.php
+++ app/importer.php
@@ -2,7 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
-class Importer {
+class WpSocialImporter {
 
 	/**
 	 * It contains the object of the importer (Facebook, Instagram or Twitter).
